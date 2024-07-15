3944d34c7d49d0db46cad018d1f325af880b9e73
kirill_khodyrev <khodyrev.k@ooostp.ru>
2024-07-15 14:56:03 +0300

    Add: manage.py: add logging to config of app

--- manage.py
+++ manage.py
@@ -1,16 +1,19 @@
 import functools
+import logging
 
 from aiohttp import web
 
+from logger import AccessLogger
 from routes import setup_routes
 from database_interaction import create_database_connection, destroy_database_connection
 
 
 async def make_app():
     app = web.Application()
+    logging.basicConfig(level=logging.INFO)
     app.on_startup.append(functools.partial(create_database_connection, db_name='orders_data.db'))
     app.on_cleanup.append(destroy_database_connection)
     setup_routes(app)
     return app
 
-web.run_app(make_app(), host='localhost', port=8080)
+web.run_app(make_app(), host='localhost', port=8080, access_log_class=AccessLogger)
