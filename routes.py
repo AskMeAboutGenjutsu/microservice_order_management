007474f142a4e9736115401ba425a234b2866483
kirill_khodyrev <khodyrev.k@ooostp.ru>
2024-07-15 12:43:25 +0300

    Add: routes.py: setup endpoint get_order

--- routes.py
+++ routes.py
@@ -1,5 +1,6 @@
-from views import post_order
+from views import post_order, get_order
 
 
 def setup_routes(app):
     app.router.add_post('/api/v1/order', post_order)
+    app.router.add_get('/api/v1/order/{id}', get_order)
