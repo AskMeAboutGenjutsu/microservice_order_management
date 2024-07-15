98dbd29e307f9ab477caae910f4369abc34e3eac
kirill_khodyrev <khodyrev.k@ooostp.ru>
2024-07-15 13:48:03 +0300

    Add: routes.py: setup endpoint patch_order

--- routes.py
+++ routes.py
@@ -1,6 +1,7 @@
-from views import post_order, get_order
+from views import post_order, get_order, patch_order
 
 
 def setup_routes(app):
     app.router.add_post('/api/v1/order', post_order)
     app.router.add_get('/api/v1/order/{id}', get_order)
+    app.router.add_patch('/api/v1/order/{id}', patch_order)
