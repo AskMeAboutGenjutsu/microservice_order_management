bfc4e2d8592e9f5aae953eecd1e45734aa1ade62
kirill_khodyrev <khodyrev.k@ooostp.ru>
2024-07-15 14:08:08 +0300

    Change: views.py: wrapp in try/except contoller 'get_order', 'patch_order'

--- views.py
+++ views.py
@@ -15,22 +15,32 @@ async def post_order(request: Request):
         await order.create()
         return web.json_response(data=order.to_dict())
     except OrderAPIException as e:
-        raise web.HTTPBadRequest(body=e.message)
+        raise web.HTTPBadRequest(text=e.message)
 
 
 async def get_order(request: Request):
-    order_id = int(request.match_info['id'])
-    db = request.app[DB_KEY]
-    order = Order(db, order_id=order_id)
-    await order.read()
-    return web.json_response(data=order.to_dict())
+    try:
+        order_id = int(request.match_info['id'])
+        db = request.app[DB_KEY]
+        order = Order(db, order_id=order_id)
+        await order.read()
+        return web.json_response(data=order.to_dict())
+    except OrderAPIException as e:
+        raise web.HTTPBadRequest(text=e.message)
+    except ValueError as e:
+        raise web.HTTPBadRequest(text=e.args[0])
 
 
 async def patch_order(request: Request):
-    order_id = int(request.match_info['id'])
-    data = await request.json()
-    db = request.app[DB_KEY]
-    order = Order(db, order_id=order_id, **data)
-    order.validate_before_update()
-    await order.update()
-    return web.json_response(data=order.to_dict())
+    try:
+        order_id = int(request.match_info['id'])
+        data = await request.json()
+        db = request.app[DB_KEY]
+        order = Order(db, order_id=order_id, **data)
+        order.validate_before_update()
+        await order.update()
+        return web.json_response(data=order.to_dict())
+    except OrderAPIException as e:
+        raise web.HTTPBadRequest(text=e.message)
+    except ValueError as e:
+        raise web.HTTPBadRequest(text=e.args[0])
