3281fbaa881c95a82d94413effee0f12490114be
kirill_khodyrev <khodyrev.k@ooostp.ru>
2024-07-15 15:02:54 +0300

    Add: create_db.py: add comments

--- create_db.py
+++ create_db.py
@@ -1,7 +1,9 @@
 import sqlite3
 
+# скрипт для создания БД sqlite
 connection = sqlite3.connect('orders_data.db')
 cursor = connection.cursor()
+# создается 2 таблицы - orders, order_product_relationship
 cursor.executescript("""
 create table orders (
 order_id integer primary key autoincrement,
