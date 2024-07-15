630c5f4c0200dcc3fdebcf77101fbd3f875e2660
kirill_khodyrev <khodyrev.k@ooostp.ru>
2024-07-15 14:01:57 +0300

    Change: views.py: wrapp in try/except contoller post_order

--- views.py
+++ views.py
@@ -3,15 +3,19 @@ from aiohttp.web_request import Request
 
 from database_interaction import DB_KEY
 from models import Order
+from order_api_exception import OrderAPIException
 
 
 async def post_order(request: Request):
-    data = await request.json()
-    db = request.app[DB_KEY]
-    order = Order(db, **data)
-    order.validate()
-    await order.create()
-    return web.json_response(data=order.to_dict())
+    try:
+        data = await request.json()
+        db = request.app[DB_KEY]
+        order = Order(db, **data)
+        order.validate()
+        await order.create()
+        return web.json_response(data=order.to_dict())
+    except OrderAPIException as e:
+        raise web.HTTPBadRequest(body=e.message)
 
 
 async def get_order(request: Request):
