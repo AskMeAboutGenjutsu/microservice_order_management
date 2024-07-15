376530666c1cd115ce8a759b6c36017224d91235
kirill_khodyrev <khodyrev.k@ooostp.ru>
2024-07-15 14:58:15 +0300

    Add: manage.py: add comments

--- manage.py
+++ manage.py
@@ -8,11 +8,16 @@ from routes import setup_routes
 from database_interaction import create_database_connection, destroy_database_connection
 
 
+# создание приложения
 async def make_app():
     app = web.Application()
+    # настройка логов
     logging.basicConfig(level=logging.INFO)
+    # при запуске подключается к sqlite
     app.on_startup.append(functools.partial(create_database_connection, db_name='orders_data.db'))
+    # при завершении отключается от sqlite
     app.on_cleanup.append(destroy_database_connection)
+    # добавление эндпоинтов
     setup_routes(app)
     return app
 
