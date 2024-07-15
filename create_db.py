722795340f81330a8f450e7c63c3157ca14c7119
kirill_khodyrev <khodyrev.k@ooostp.ru>
2024-07-15 11:48:57 +0300

    Change create db script

--- create_db.py
+++ create_db.py
@@ -3,21 +3,11 @@ import sqlite3
 connection = sqlite3.connect('orders_data.db')
 cursor = connection.cursor()
 cursor.executescript("""
-create table user (
-user_id integer primary key autoincrement,
-name text not null,
-email text not null);
-
 create table orders (
 order_id integer primary key autoincrement,
 user_id integer not null,
 status text not null check (status in ('accepted', 'delivery', 'finalised')) default 'accepted',
 foreign key (user_id) references user (user_id));
-
-create table product (
-product_id integer primary key autoincrement,
-description text not null,
-price real not null);
 
 create table order_product_relationship (
 order_product_relationship_id integer primary key autoincrement,
