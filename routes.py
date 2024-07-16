335f694f1543224e25ddb07dd5b1e5668ea32a48
kirill_khodyrev <khodyrev.k@ooostp.ru>
2024-07-16 08:09:20 +0300

    Add: routes.py: comments

--- routes.py
+++ routes.py
@@ -1,6 +1,7 @@
 from views import post_order, get_order, patch_order
 
 
+# установка апи эндпоинтов
 def setup_routes(app):
     app.router.add_post('/api/v1/order', post_order)
     app.router.add_get('/api/v1/order/{id}', get_order)
